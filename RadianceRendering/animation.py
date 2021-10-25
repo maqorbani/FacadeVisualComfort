12c4bd739e4e00d32e7d026a9dc843f3e675d717
MohammadAli Qorbani <qorbani.maq@gmail.com>
2021-10-25 11:51:31 +0330

    octree file name was modified

--- RadianceRendering/animation.py
+++ RadianceRendering/animation.py
@@ -3,6 +3,6 @@ import os
 import time
 
 for i in sorted(os.listdir('skies')):
-    os.system(f'oconv skies/{i} Motahareh_ahmadi.rad > 123.oct')
-    time.sleep(0.02)
+    os.system(f'oconv skies/{i} Geo.rad > 123.oct')
+    time.sleep(0.1)
     print(i)
